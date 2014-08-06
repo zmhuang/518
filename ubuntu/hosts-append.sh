ec77a27d6da22cc97f660f7e800765dc98d644d1
zmhuang <zm-huang@126.com>
2014-08-06 21:38:15 +0800

    c

--- ubuntu/hosts-append.sh
+++ ubuntu/hosts-append.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+FILE=/tmp/hosts
+
 /bin/cp /etc/hosts /tmp/ -v
-cat hosts-append >> /tmp/hosts
+cat hosts-append >> $FILE
+
+hn=`hostname`
+echo "127.0.0.1\t$hn" >> $hn
+
 sudo cp /tmp/hosts /etc/hosts
