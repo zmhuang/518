a6ee1704987d8beb4cbab79a43df07bbede1ed51
zmhuang <zm-huang@126.com>
2014-08-10 17:09:39 +0800

    x

--- ubuntu/hosts-append.sh
+++ ubuntu/hosts-append.sh
@@ -8,7 +8,7 @@ FILE=hosts
 
 # 1、下载 hosts 文件
 #curl 'http://bj2.dl.baidupcs.com/file/6133b560d7b2f6620895e04564b2285f?fid=33631425-250528-938598892735827&time=1407465395&sign=FDTAXER-DCb740ccc5511e5e8fedcff06b081203-GRXbZMGrAWyvh4cl%2BXSGGuz59f0%3D&to=abp2&fm=Nin,B,U,nc&newver=1&newfm=1&flow_ver=3&expires=1407465995&rt=sh&r=332838405&mlogid=3231459272&sh=1&vuk=-&vbdid=481974168&fn=imouto.host.7z' > hosts.7z
-7z e -y hosts.7z
+7z e -y imouto.host.7z
 mv imouto.host.txt $FILE
 
 echo >> $FILE
