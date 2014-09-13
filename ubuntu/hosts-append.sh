2d063cc6af64424daa341f873b1b26aa1d74ff7b
zmhuang <zm-huang@126.com>
2014-09-13 22:48:39 +0800

    x

--- ubuntu/hosts-append.sh
+++ ubuntu/hosts-append.sh
@@ -10,6 +10,7 @@ FILE=hosts
 #curl 'http://bj2.dl.baidupcs.com/file/6133b560d7b2f6620895e04564b2285f?fid=33631425-250528-938598892735827&time=1407465395&sign=FDTAXER-DCb740ccc5511e5e8fedcff06b081203-GRXbZMGrAWyvh4cl%2BXSGGuz59f0%3D&to=abp2&fm=Nin,B,U,nc&newver=1&newfm=1&flow_ver=3&expires=1407465995&rt=sh&r=332838405&mlogid=3231459272&sh=1&vuk=-&vbdid=481974168&fn=imouto.host.7z' > hosts.7z
 7z e -y imouto.host.7z
 mv imouto.host.txt $FILE
+fromdos $FILE
 
 echo >> $FILE
 
@@ -23,7 +24,10 @@ do
 done
 
 IP=`grep -m 1 www.googleusercontent.com hosts | cut -f 1`
-echo -e "${IP}\twww-trixcopysheet-opensocial.googleusercontent.com" >> $FILE
+for i in doc-0o-0o-docs.googleusercontent.com www-trixcopysheet-opensocial.googleusercontent.com;
+do
+echo -e "${IP}\t${i}" >> $FILE
+done
 
 hn=`hostname`
 echo -e "127.0.0.1\t$hn" >> $FILE
