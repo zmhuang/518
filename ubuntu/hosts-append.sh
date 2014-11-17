cbb683ca78fce81272dfe0e318cdbb744345ba98
zmhuang <bf533@126.com>
2014-11-17 19:02:52 +0800

    x

--- ubuntu/hosts-append.sh
+++ ubuntu/hosts-append.sh
@@ -10,7 +10,7 @@ fromdos $FILE
 echo >> $FILE
 
 # 2、添加一些条目
-IP=`grep -m 1 googlesource.com hosts | cut -f 1`
+IP=`grep -m 1 googlesource.com hosts | sed 's/\([0-9.]\+\).*/\1/'`
 for i in android.googlesource.com gerrit.googlesource.com gerrit-documentation.storage.googleapis.com fonts.gstatic.com;
 do
     if ! grep -q $i $FILE; then
@@ -18,16 +18,17 @@ do
     fi
 done
 
-IP=`grep -m 1 www.googleusercontent.com hosts | cut -f 1`
+IP=`grep -m 1 www.googleusercontent.com hosts | sed 's/\([0-9.]\+\).*/\1/'`
 for i in doc-08-0o-docs.googleusercontent.com doc-0o-0o-docs.googleusercontent.com www-trixcopysheet-opensocial.googleusercontent.com;
 do
 echo -e "${IP}\t${i}" >> $FILE
 done
 
-IP=`grep -m 1 0.docs.google.com hosts | cut -f 1`
+IP=`grep -m 1 0.docs.google.com hosts | sed 's/\([0-9.]\+\).*/\1/'`
 for i in {10..50};
 do
 echo -e "${IP}\t${i}.docs.google.com" >> $FILE
+((++i))
 done
 
 hn=`hostname`
