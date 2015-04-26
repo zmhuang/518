3ad115b82bb78ad5c6873bc76ffbe844439bbf04
huangzhimin <zm-huang@126.com>
2015-04-26 09:49:41 +0800

    x

--- android/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-export JAVA_HOME=$HOME/jdk1.8.0_45
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/sbin:$HOME/android-studio/bin:$HOME/Android/Sdk/tools:$HOME/jdk1.8.0_45/bin
-studio.sh
-#android
